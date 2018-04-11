3fda3f683f86e2d52aad7033171d218ab6e887d5
danimlds <daniel.mlds@gmail.com>
2018-04-10 22:05:58 -0300

    tela de cadastro usando c# e banco access

---
+++ cadastroCliente/cadastroCliente/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace cadastroCliente
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_endereco = new System.Windows.Forms.Label();
+            this.lbl_cidade = new System.Windows.Forms.Label();
+            this.lbl_estado = new System.Windows.Forms.Label();
+            this.lbl_rg = new System.Windows.Forms.Label();
+            this.lbl_cpf = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_cidade = new System.Windows.Forms.TextBox();
+            this.txt_estado = new System.Windows.Forms.TextBox();
+            this.txt_rg = new System.Windows.Forms.TextBox();
+            this.txt_cpf = new System.Windows.Forms.TextBox();
+            this.txt_endereco = new System.Windows.Forms.TextBox();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.btn_salvar = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_consulta = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Location = new System.Drawing.Point(51, 47);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(19, 13);
+            this.lbl_id.TabIndex = 0;
+            this.lbl_id.Text = "Id:";
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(51, 100);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(38, 13);
+            this.lbl_nome.TabIndex = 1;
+            this.lbl_nome.Text = "Nome:";
+            // 
+            // lbl_endereco
+            // 
+            this.lbl_endereco.AutoSize = true;
+            this.lbl_endereco.Location = new System.Drawing.Point(51, 147);
+            this.lbl_endereco.Name = "lbl_endereco";
+            this.lbl_endereco.Size = new System.Drawing.Size(56, 13);
+            this.lbl_endereco.TabIndex = 2;
+            this.lbl_endereco.Text = "Endereço:";
+            // 
+            // lbl_cidade
+            // 
+            this.lbl_cidade.AutoSize = true;
+            this.lbl_cidade.Location = new System.Drawing.Point(51, 194);
+            this.lbl_cidade.Name = "lbl_cidade";
+            this.lbl_cidade.Size = new System.Drawing.Size(43, 13);
+            this.lbl_cidade.TabIndex = 3;
+            this.lbl_cidade.Text = "Cidade:";
+            // 
+            // lbl_estado
+            // 
+            this.lbl_estado.AutoSize = true;
+            this.lbl_estado.Location = new System.Drawing.Point(51, 240);
+            this.lbl_estado.Name = "lbl_estado";
+            this.lbl_estado.Size = new System.Drawing.Size(43, 13);
+            this.lbl_estado.TabIndex = 4;
+            this.lbl_estado.Text = "Estado:";
+            // 
+            // lbl_rg
+            // 
+            this.lbl_rg.AutoSize = true;
+            this.lbl_rg.Location = new System.Drawing.Point(51, 290);
+            this.lbl_rg.Name = "lbl_rg";
+            this.lbl_rg.Size = new System.Drawing.Size(24, 13);
+            this.lbl_rg.TabIndex = 5;
+            this.lbl_rg.Text = "Rg:";
+            // 
+            // lbl_cpf
+            // 
+            this.lbl_cpf.AutoSize = true;
+            this.lbl_cpf.Location = new System.Drawing.Point(51, 336);
+            this.lbl_cpf.Name = "lbl_cpf";
+            this.lbl_cpf.Size = new System.Drawing.Size(25, 13);
+            this.lbl_cpf.TabIndex = 6;
+            this.lbl_cpf.Text = "cpf:";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(166, 40);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 7;
+            // 
+            // txt_cidade
+            // 
+            this.txt_cidade.Location = new System.Drawing.Point(166, 187);
+            this.txt_cidade.Name = "txt_cidade";
+            this.txt_cidade.Size = new System.Drawing.Size(100, 20);
+            this.txt_cidade.TabIndex = 8;
+            // 
+            // txt_estado
+            // 
+            this.txt_estado.Location = new System.Drawing.Point(166, 233);
+            this.txt_estado.Name = "txt_estado";
+            this.txt_estado.Size = new System.Drawing.Size(100, 20);
+            this.txt_estado.TabIndex = 9;
+            // 
+            // txt_rg
+            // 
+            this.txt_rg.Location = new System.Drawing.Point(166, 283);
+            this.txt_rg.Name = "txt_rg";
+            this.txt_rg.Size = new System.Drawing.Size(100, 20);
+            this.txt_rg.TabIndex = 10;
+            // 
+            // txt_cpf
+            // 
+            this.txt_cpf.Location = new System.Drawing.Point(166, 329);
+            this.txt_cpf.Name = "txt_cpf";
+            this.txt_cpf.Size = new System.Drawing.Size(100, 20);
+            this.txt_cpf.TabIndex = 11;
+            // 
+            // txt_endereco
+            // 
+            this.txt_endereco.Location = new System.Drawing.Point(166, 140);
+            this.txt_endereco.Name = "txt_endereco";
+            this.txt_endereco.Size = new System.Drawing.Size(100, 20);
+            this.txt_endereco.TabIndex = 12;
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(166, 93);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 13;
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.Location = new System.Drawing.Point(14, 386);
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(75, 23);
+            this.btn_salvar.TabIndex = 14;
+            this.btn_salvar.Text = "Salvar";
+            this.btn_salvar.UseVisualStyleBackColor = true;
+            this.btn_salvar.Click += new System.EventHandler(this.btn_salvar_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(95, 386);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpar.TabIndex = 15;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_consulta
+            // 
+            this.btn_consulta.Location = new System.Drawing.Point(176, 386);
+            this.btn_consulta.Name = "btn_consulta";
+            this.btn_consulta.Size = new System.Drawing.Size(75, 23);
+            this.btn_consulta.TabIndex = 16;
+            this.btn_consulta.Text = "Consultar";
+            this.btn_consulta.UseVisualStyleBackColor = true;
+            this.btn_consulta.Click += new System.EventHandler(this.btn_consulta_Click);
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(257, 386);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_excluir.TabIndex = 17;
+            this.btn_excluir.Text = "Excluir:";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(338, 386);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_sair.TabIndex = 18;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 415);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_consulta);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_salvar);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.txt_endereco);
+            this.Controls.Add(this.txt_cpf);
+            this.Controls.Add(this.txt_rg);
+            this.Controls.Add(this.txt_estado);
+            this.Controls.Add(this.txt_cidade);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.lbl_cpf);
+            this.Controls.Add(this.lbl_rg);
+            this.Controls.Add(this.lbl_estado);
+            this.Controls.Add(this.lbl_cidade);
+            this.Controls.Add(this.lbl_endereco);
+            this.Controls.Add(this.lbl_nome);
+            this.Controls.Add(this.lbl_id);
+            this.Name = "Formulario";
+            this.Text = "Formulario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_endereco;
+        private System.Windows.Forms.Label lbl_cidade;
+        private System.Windows.Forms.Label lbl_estado;
+        private System.Windows.Forms.Label lbl_rg;
+        private System.Windows.Forms.Label lbl_cpf;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_cidade;
+        private System.Windows.Forms.TextBox txt_estado;
+        private System.Windows.Forms.TextBox txt_rg;
+        private System.Windows.Forms.TextBox txt_cpf;
+        private System.Windows.Forms.TextBox txt_endereco;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Button btn_salvar;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_consulta;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_sair;
+    }
+}
+
---
+++ cadastroCliente/cadastroCliente/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Data.OleDb; 
+
+namespace cadastroCliente
+{
+    public partial class Formulario : Form
+    {
+        //string de conexão ATENÇÂO !!! substituir \ por \\ 
+        static string strCn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Aluno_2\\Downloads\\cliente.accdb";
+
+        OleDbConnection conexao = new OleDbConnection(strCn);
+
+        public Formulario()
+
+        {
+            InitializeComponent();
+        }
+
+        private void btn_consulta_Click(object sender, EventArgs e)
+        {
+            //instrução sql responsável por pesquisar o banco de dados (CRUD - Read)
+            string pesquisa = "select * from cadastroCliente where Id = " + txt_id.Text;
+            //criando um objeto de nome cmd tendo como modelo a classe OleDbCommand para executar a instrução sql
+            OleDbCommand cmd = new OleDbCommand(pesquisa, conexao);
+            // Atravé da classe OleDbDataReader que faz parte do SqlCliente, criamos uma //variável chamada DR que será usada na leitura dos dados (instrução select)
+            OleDbDataReader DR;
+            //tratamento de exceções: try - catch - finally (em caso de erro capturamos o //tipo do erro)
+
+try 
+{
+// Abrindo a conexão com o banco
+conexao.Open();
+// Executando a instrução e armazenando o resultado no reader DR
+DR = cmd.ExecuteReader();
+// Se houver um registro correspondente ao Id
+if (DR.Read()) 
+
+{ 
+// Exibe as informações nas caixas de texto (textBox) correspondentes (0) //corresponde ao Id, (1) ao Nome e assim sucessivamente 
+txt_id.Text = DR.GetValue(0).ToString(); 
+txt_nome.Text = DR.GetValue(1).ToString(); 
+txt_endereco.Text = DR.GetValue(2).ToString(); 
+txt_cidade.Text = DR.GetValue(3).ToString();
+txt_estado.Text = DR.GetValue(4).ToString();
+txt_rg.Text = DR.GetValue(5).ToString();
+txt_cpf.Text = DR.GetValue(6).ToString(); 
+} 
+
+// Senão, exibimos uma mensagem avisando e também limpamos os campos para uma //nova pesquisa 
+else 
+{ 
+MessageBox.Show("Registro não encontrado"); 
+txt_nome.Clear(); 
+txt_endereco.Clear(); 
+txt_cidade.Clear(); 
+txt_estado.Clear();
+txt_rg.Clear();
+txt_cpf.Clear();
+txt_id.Focus(); 
+
+} // Encerrando o uso do reader DR 
+DR.Close(); 
+
+// Encerrando o uso do cmd 
+cmd.Dispose(); 
+} 
+
+//caso ocorra algum erro 
+catch (Exception ex) 
+{
+ 
+//exiba qual é o erro 
+MessageBox.Show(ex.Message); 
+} 
+
+// de qualquer forma sempre fechar a conexão com o banco ("lembrar da porta da //geladeira rsrsrs") 
+finally 
+{ 
+conexao.Close(); 
+} 
+}
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            //instrução sql responsável por adicionar dados ao banco (CRUD - Create) 
+string adiciona = "insert into cadastroCliente values (" + 
+txt_id.Text + ",'" + 
+txt_nome.Text + "','" + 
+txt_endereco.Text + "','" +
+txt_cidade.Text + "','" +
+txt_estado.Text + "','" +
+txt_rg.Text + "','" +
+txt_cpf.Text + "')"; 
+
+//criando um objeto de nome cmd tendo como modelo a classe OleDbCommand para //executar a instrução sql 
+OleDbCommand cmd = new OleDbCommand(adiciona, conexao);
+ 
+//tratamento de exceções: try - catch - finally (em caso de erro capturamos o //tipo do erro) 
+try 
+{ 
+// Abrindo a conexão com o banco 
+conexao.Open(); 
+
+// Criando uma variável para adicionar e armazenar o resultado 
+int resultado;
+resultado = cmd.ExecuteNonQuery(); 
+
+// Verificando se o registro foi adicionado 
+// Caso o valor da variável resultado seja 1 
+// significa que o comando funcionou, neste caso limpar os campos e exibir uma //mensagem 
+if (resultado == 1) 
+{ 
+MessageBox.Show("Registro adicionado com sucesso"); 
+txt_nome.Clear(); 
+txt_endereco.Clear(); 
+txt_cidade.Clear(); 
+txt_estado.Clear();
+txt_rg.Clear();
+txt_cpf.Clear();
+txt_id.Focus(); 
+}
+ 
+// Encerrando o uso do cmd 
+cmd.Dispose(); 
+}
+ 
+//caso ocorra algum erro 
+catch (Exception ex) 
+{
+ 
+//exiba qual é o erro 
+MessageBox.Show(ex.Message); 
+} 
+
+// de qualquer forma sempre fechar a conexão com o banco ("lembrar da porta da //geladeira rsrsrs") 
+finally 
+{ 
+conexao.Close(); 
+} 
+}
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            //instrução sql responsável por remover um registro do banco (CRUD - Delete) 
+string remove = "delete from cadastroCliente where Id= " + txt_id.Text; 
+
+//criando um objeto de nome cmd tendo como modelo a classe OleDbCommand para //executar a instrução sql 
+OleDbCommand cmd = new OleDbCommand(remove, conexao); 
+
+//tratamento de exceções: try - catch - finally (em caso de erro capturamos o //tipo do erro) 
+try 
+{ 
+
+// Abrindo a conexão com o banco 
+conexao.Open(); 
+
+// Criando uma variável para adicionar e armazenar o resultado 
+int resultado; 
+if (MessageBox.Show("Tem certeza que deseja remover este registro ?", "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) 
+{ 
+resultado = cmd.ExecuteNonQuery();
+// Verificando se o registro foi apagado 
+// Caso o valor da variável resultado seja 1 
+// significa que o comando funcionou, neste caso limpar os campos e exibir uma //mensagem 
+if (resultado == 1) 
+{ 
+txt_nome.Clear(); 
+txt_endereco.Clear(); 
+txt_cidade.Clear(); 
+txt_estado.Clear();
+txt_rg.Clear();
+txt_cpf.Clear();
+txt_id.Focus(); 
+MessageBox.Show("Registro removido com sucesso"); 
+} 
+
+// Encerrando o uso do cmd 
+cmd.Dispose(); 
+} 
+} 
+
+//caso ocorra algum erro 
+catch (Exception ex) 
+{ 
+
+//exiba qual é o erro 
+MessageBox.Show(ex.Message); 
+} 
+// de qualquer forma sempre fechar a conexão com o banco 
+finally 
+{ 
+conexao.Close(); 
+} 
+}
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_id.Clear();
+            txt_nome.Clear();
+            txt_endereco.Clear();
+            txt_cidade.Clear();
+            txt_estado.Clear();
+            txt_rg.Clear();
+            txt_cpf.Clear();}
+    }
+        }
